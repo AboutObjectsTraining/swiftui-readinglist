c9b2ae666cc6cf8f0b1b1cebffa85b1177fe42ed
Jonathan Lehr <jonathan@aboutobjects.com>
2019-12-16 14:01:35 -0500

    Initial Commit

---
+++ ReadingList/ContentView.swift
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
